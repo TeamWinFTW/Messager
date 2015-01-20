f4fd3d07fa902056cf93da740bbe93312c10b3ee
nodstuff <tom.meaney@mycit.ie>
2015-01-20 01:49:21 +0000

    First commit

--- app/src/main/java/ie/nodstuff/messager/NewMessageActivity.java
+++ app/src/main/java/ie/nodstuff/messager/NewMessageActivity.java
@@ -6,16 +6,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
 
 
 public class NewMessageActivity extends ActionBarActivity {
